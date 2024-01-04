a335e618e51e7b9787084169b953d146886546f5
Tiago Cordeiro <53503942+tiagocrodeiro@users.noreply.github.com>
2024-01-04 00:54:13 -0300

    Update mesclador_de_arquivos.py

--- mesclador_de_arquivos.py
+++ mesclador_de_arquivos.py
@@ -4,17 +4,20 @@ import warnings
 from tkinter.filedialog import askdirectory
 
 warnings.simplefilter("ignore")
+lista = []
+
+#CRIA UMA JANELA DE DIALOGO PARA SELECIONAR O DIRETÓRIO E SALVA O SEU CAMINHO NA VARIAVEL 
 caminho = askdirectory(title='Escolha o diretório')
 
+#LISTA TODOS OS ARQUIVOS QUE ESTÃO NO DIRETORIO INDICADO
 lista_de_arquivo = os.listdir(caminho)
 
-lista = []
-#print(lista_de_arquivo)
+#LAÇO PARA IDENTIFICAR OS ARQUIVOS E ADICIONALOS NA LISTA, UTILIZANDO PANDAS PARA LER OS ARQUIVOS
 for file in lista_de_arquivo:
    if file.endswith('.xlsx'):
        lista.append(pd.read_excel(file))
 
 
-
+#CONCATENANDO OS ARQUIVOS E GERANDO O ARQUIVO .csv
 dado = pd.concat(lista)
-dado.to_csv('caminho')
+dado.to_csv('caminho.csv')
